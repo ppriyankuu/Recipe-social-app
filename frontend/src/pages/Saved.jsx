6d156eb030066d7c01b3deed827567c7c61ff9c6
Priyanku Gogoi <114563767+ppriyankuu@users.noreply.github.com>
2024-11-21 09:34:06 +0530

    Saved.jsx

--- frontend/src/pages/Saved.jsx
+++ frontend/src/pages/Saved.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useGetUserId } from "../hooks/useGetUserId";
